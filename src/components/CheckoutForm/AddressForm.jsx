df92cad375dfb5e247012006a67539646d43e773
SmartCode-site <git config --global user.email web_developer13@mail.ru>
2021-04-28 21:06:34 +0500

    first commit

---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,170 @@
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+import { commerce } from "../../lib/commerce";
+import FormInput from "./CustomTextField";
+
+
+
+const AddressForm = ({ checkoutToken, test }) => {
+    
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCountry, setShippingCountry] = useState("");
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState("");
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState("");
+  const methods = useForm();
+
+  const fetchShippingCountries = async (checkoutTokenId) => {
+    const { countries } = await commerce.services.localeListShippingCountries(
+      checkoutTokenId
+    );
+
+    setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(
+      countryCode
+    );
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
+  };
+
+  const fetchShippingOptions = async (
+    checkoutTokenId,
+    country,
+    stateProvince = null
+  ) => {
+    const options = await commerce.checkout.getShippingOptions(
+      checkoutTokenId,
+      { country, region: stateProvince }
+    );
+
+    setShippingOptions(options);
+    setShippingOption(options[0].id);
+  };
+
+  useEffect(() => {
+    fetchShippingCountries(checkoutToken.id);
+  }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
+
+  useEffect(() => {
+    if (shippingSubdivision)
+      fetchShippingOptions(
+        checkoutToken.id,
+        shippingCountry,
+        shippingSubdivision
+      );
+  }, [shippingSubdivision]);
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <FormProvider {...methods}>
+        <form 
+          onSubmit={methods.handleSubmit((data) =>
+            test({
+              ...data,
+              shippingCountry,
+              shippingSubdivision,
+              shippingOption,
+            })
+          )}
+        >
+          <Grid container spacing={3}>
+            <TextField name="firstName" label="First name"  style={{marginLeft:"20px"}}/>
+            <TextField name="lastName" label="Last name" style={{marginLeft:"20px"}}/>
+            <TextField name="address1" label="Address line 1" style={{marginLeft:"20px"}}/>
+            <TextField name="email" label="Email" style={{marginLeft:"20px"}}/>
+            <TextField name="city" label="City" style={{marginLeft:"20px"}}/>
+            <TextField name="zip" label="Zip / Postal code" style={{marginLeft:"20px"}}/>
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select
+                value={shippingCountry}
+                fullWidth
+                onChange={(e) => setShippingCountry(e.target.value)}
+              >
+                {Object.entries(shippingCountries)
+                  .map(([code, name]) => ({ id: code, label: name }))
+                  .map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivision</InputLabel>
+              <Select
+                value={shippingSubdivision}
+                fullWidth
+                onChange={(e) => setShippingSubdivision(e.target.value)}
+              >
+                {Object.entries(shippingSubdivisions)
+                  .map(([code, name]) => ({ id: code, label: name }))
+                  .map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select
+                value={shippingOption}
+                fullWidth
+                onChange={(e) => setShippingOption(e.target.value)}
+              >
+                {shippingOptions
+                  .map((sO) => ({
+                    id: sO.id,
+                    label: `${sO.description} - (${sO.price.formatted_with_symbol})`,
+                  }))
+                  .map((item) => (
+                    <MenuItem key={item.id} value={item.id}>
+                      {item.label}
+                    </MenuItem>
+                  ))}
+              </Select>
+            </Grid>
+          </Grid>
+          <br />
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button component={Link} variant="outlined" to="/cart">
+              Back to Cart
+            </Button>
+            <Button type="submit" variant="contained" color="primary">
+              Next
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default AddressForm;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import Product from './Product/Product'
+
+
+const Products = ({products ,onAddToCart}) => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4} style={{marginTop:"100px"}}>
+                {products.map((product)=>(
+                    <Grid item key={product.id} xs={12} sm={6} md={4} ld={3}>
+                        <Product product={product}  onAddToCart={onAddToCart}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
